7ab55a311d0a2f71890988ff07a740fed351eec5
Sagar G <sagar.gaigawale@noustek.com>
2023-05-01 19:37:52 +0530

    initial commit

---
+++ src/main/java/com/bitsbank/accounts/controller/AccountsController.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.bitsbank.accounts.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bitsbank.accounts.model.Accounts;
+import com.bitsbank.accounts.model.Customer;
+import com.bitsbank.accounts.repository.AccountsRepository;
+
+/**
+ * @author Eazy Bytes
+ *
+ */
+
+@RestController
+public class AccountsController {
+	
+	@Autowired
+	private AccountsRepository accountsRepository;
+
+	@PostMapping("/myAccount")
+	public Accounts getAccountDetails(@RequestBody Customer customer) {
+
+		Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
+		if (accounts != null) {
+			return accounts;
+		} else {
+			return null;
+		}
+
+	}
+
+}
